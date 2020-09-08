20c6a87dd019940d14810c8737a7f253d476d064
Katie B <katiebittner1@gmail.com>
2020-09-08 21:18:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  organized_pigeons = data.each_with_object({}) do |(attribute_category, attribute_list), organized_pigeons|
+
+    attribute_list.each do |attribute, pigeons|   
+
+      pigeons.each do |pigeon|
+      
+        if !organized_pigeons[pigeon]
+          organized_pigeons[pigeon] = {}
+        end
+        
+        if !organized_pigeons[pigeon][attribute_category]
+          organized_pigeons[pigeon][attribute_category] = []
+        end
+        
+        organized_pigeons[pigeon][attribute_category].push(attribute.to_s)
+        
+      end 
+    end 
+  end 
 end
