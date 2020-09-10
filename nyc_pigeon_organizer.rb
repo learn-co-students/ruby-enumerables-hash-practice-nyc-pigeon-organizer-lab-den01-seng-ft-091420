81f1913bdd082f15b94bde36f4f369c817d1ae36
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-10 20:31:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  final_organizer = data.each_with_object({}) do |(key, val), organizer|
+    #binding.pry
+    val.each do |color, birds|
+      birds.each do |name|
+        #binding.pry
+        if !organizer[name]
+          organizer[name] = {}
+        end
+        if !organizer[name][key]
+          !organizer[name][key] = []
+        end
+        organizer[name][key].push(color.to_s)
+      end
+    end
+  end
 end
